fff5c70a1c2b3fdbd63e687ed1625292be16cec2
Himanshu Rajauria <hrajauria1997@gmail.com>
2023-10-22 03:29:18 +0530

    Changes done

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,54 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
+    public String getName (){
+        return name;
+    }
 
-        return null;
+    public double getBalance(){
+        return  balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public String generateAccountNumber(int digits, int sum) throws Exception {
+        if (digits <= 0 || sum < 0 || sum > 9 * digits) {
+            throw new Exception("Account Number cannot be generated");
+        }
+
+        StringBuilder accountNumber = new StringBuilder();
+        int remainingSum = sum;
+
+        for (int i = 0; i < digits - 1; i++) {
+            int maxPossibleDigit = Math.min(remainingSum, 9);
+            int randomDigit = (int) (Math.random() * (maxPossibleDigit + 1));
+            accountNumber.append(randomDigit);
+            remainingSum -= randomDigit;
+        }
+
+        accountNumber.append(remainingSum);
+
+        return accountNumber.toString();
     }
 
     public void deposit(double amount) {
-        //add amount to balance
-
+        // Add amount to balance
+        this.balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        if (this.balance - amount > minBalance) {
+            this.balance -= amount;
+        }
+        else throw new Exception("Insufficient Balance");
+        // Withdraw the specified amount
 
     }
-
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -4,8 +4,13 @@ public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+        super (name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
+        if (balance < 5000){
+            throw new Exception("Insufficient Balance");
+        }
+        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +19,61 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        if (!isValid(tradeLicenseId)){
+            String rearranged = rearrange (tradeLicenseId);
+            if (rearranged.isEmpty()) throw new Exception("Valid License can not be generated");
+            else this.tradeLicenseId = rearranged;
+        }
+
+
+
+    }
+
+    public boolean isValid (String licenseId){
+        for(int i=0; i<licenseId.length()-1; i++){
+            if(licenseId.charAt(i) == licenseId.charAt(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String rearrange (String id){
+        int len = id.length();
+        int[] hash = new int [26];
+        for (int i = 0 ; i < len;i++){
+            hash[id.charAt(len) - 'a']++;
+        }
+
+        int max = 0;
+        int letter = 0;
+        for (int i = 0 ; i < 26 ; i++){
+            if (hash[i] > max){
+                max = hash[i];
+                letter = i;
+            }
+        }
+
+        if (max > ((len + 1) / 2)) return "";
+
+        int idx  = 0;
+        char[] arr = new char [len];
+
+
+        while (hash [letter] -- > 0){
+            arr[idx] = (char)(letter + 'a');
+            idx += 2;
+        }
+
+        for (int i = 0 ; i < 26; i++){
+            while (hash[i]-- > 0) {
+                if (idx > arr.length) idx = 1;
+                arr[idx] = (char) (i + 'a');
+                idx += 2;
+            }
+        }
+
+        return String.valueOf(arr);
     }
 
 }
